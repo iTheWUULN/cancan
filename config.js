daf32eda35ec0cded9af7f651cbd2e294b1c92f5
iTheWUULN <96634321+iTheWUULN@users.noreply.github.com>
2024-02-01 21:15:24 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    embed: {
+        color: "#fee10c",
+        footer: (footer) => `${footer.username} tarafından istedindi.`
+    },
+    emoji: {}
+
+},
+
+{
+    "TOKEN": "MTIwMjY2OTMwOTU2Mjk3ODMyNQ.Ge2NF7.KuX6jIItTI7hYYGW9pat7yG5KD1GyizWNxjjSk"
+}
